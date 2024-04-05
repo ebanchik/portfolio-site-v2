5d25f657847a791e83fc0d4858fe68b214c34032
ebanchik <ebanchik@gmail.com>
2024-04-05 15:11:41 -0400

    animate E

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import { Banner } from './Banner'
+// import { Banner } from './Banner'
+import { MyName } from './Banner'
 
 function App() {
   return (
   <div>
-    <Banner />
+    <MyName />
   </div>
   )
 }
--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,6 +1,21 @@
-export function Banner() {
+import { motion } from 'framer-motion';
 
+export const MyName = () => {
   return (
-    <h1>HELLOOOOO</h1>
-  )
-  }
+    <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 179.23 251.92" style={{ width: '200px', height: 'auto' }}>
+        <motion.path
+          fill="none"
+          stroke="black"
+          strokeWidth="10"
+          d="m0,0h171.82v45.16H52.57v54.69h112.2v45.16H52.57v61.74h126.66l-5.29,45.16H0V0Z"
+          initial={{ pathLength: 0 }}
+          animate={{ pathLength: 1 }}
+          transition={{ duration: 2 }}
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default MyName;
