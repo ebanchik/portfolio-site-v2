0123d8355140ce2bedc5c77c90ea1e8d02b63e6e
ebanchik <ebanchik@gmail.com>
2024-04-05 15:50:17 -0400

    better E animation

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,17 +1,36 @@
 import { motion } from 'framer-motion';
 
 export const MyName = () => {
+  const draw = {
+    hidden: { pathLength: 0, opacity: 0 },
+    visible: {
+      pathLength: 1,
+      opacity: 1,
+      transition: { duration: 2 }
+    }
+  };
+
   return (
     <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 179.23 251.92" style={{ width: '200px', height: 'auto' }}>
-        <motion.path
-          fill="none"
-          stroke="black"
-          strokeWidth="10"
-          d="m0,0h171.82v45.16H52.57v54.69h112.2v45.16H52.57v61.74h126.66l-5.29,45.16H0V0Z"
-          initial={{ pathLength: 0 }}
-          animate={{ pathLength: 1 }}
-          transition={{ duration: 2 }}
+      <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 197.48 335.98" style={{ width: '200px', height: 'auto' }}>
+        <motion.polyline
+          className="cls-1"
+          points="194.55 31.5 39 31.5 39 132.85 39 241.86 197.48 241.86"
+          style={{ fill: 'none', stroke: '#000', strokeWidth: '63' }}
+          variants={draw}
+          initial="hidden"
+          animate="visible"
+        />
+        <motion.line
+          className="cls-1"
+          x1="186"
+          y1="129.8"
+          x2="39"
+          y2="129.8"
+          style={{ fill: 'none', stroke: '#000', strokeWidth: '63' }}
+          variants={draw}
+          initial="hidden"
+          animate="visible"
         />
       </svg>
     </div>
