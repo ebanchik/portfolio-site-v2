de0a64867ca9bf7e72a61c9565e1c86c7e2443ec
ebanchik <ebanchik@gmail.com>
2024-04-06 23:32:11 +0100

    animation + correct font

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,38 +1,31 @@
 import { motion } from 'framer-motion';
 
 export const MyName = () => {
-  const draw = {
-    hidden: { pathLength: 0, opacity: 0 },
-    visible: {
-      pathLength: 1,
-      opacity: 1,
-      transition: { duration: 2 }
-    }
+
+  const reveal = {
+    hidden: { scaleX: 0, transformOrigin: 'left' },
+    visible: { 
+      scaleX: 1, 
+      transition: { duration: 2, ease: 'easeInOut' }
+    },
   };
 
   return (
-    <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 197.48 335.98" style={{ width: '200px', height: 'auto' }}>
-        <motion.polyline
-          className="cls-1"
-          points="194.55 31.5 39 31.5 39 132.85 39 241.86 197.48 241.86"
-          style={{ fill: 'none', stroke: '#000', strokeWidth: '63' }}
-          variants={draw}
-          initial="hidden"
-          animate="visible"
-        />
-        <motion.line
-          className="cls-1"
-          x1="186"
-          y1="129.8"
-          x2="39"
-          y2="129.8"
-          style={{ fill: 'none', stroke: '#000', strokeWidth: '63' }}
-          variants={draw}
-          initial="hidden"
-          animate="visible"
-        />
-      </svg>
+    <div style={{ width: '435.5px', height: '337.16px', overflow: 'hidden' }}>
+      <motion.svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 435.5 337.16"
+        initial="hidden"
+        animate="visible"
+        style={{ originX: 0 }} // Ensure scaling happens left to right
+      >
+        <defs>
+          <motion.clipPath id="clip" variants={reveal}>
+            <rect width="435.5" height="337.16" />
+          </motion.clipPath>
+        </defs>
+        <g clipPath="url(#clip)">
+          <path fill="black" d="m234.33,0h192.5v50.6h-133.61v61.27h125.7v50.6h-125.7v69.17h141.91l-5.93,50.6h-194.88V0Z" />
+        </g>
+      </motion.svg>
     </div>
   );
 };
