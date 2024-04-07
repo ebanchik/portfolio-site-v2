d224989ddf6d86f1d27108010762dbbebabfdee7
ebanchik <ebanchik@gmail.com>
2024-04-07 17:24:49 +0100

    initial

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -7,6 +7,9 @@ export const Banner = () => {
       <h1 className="tagline">FRONT END DEVELOPER WITH A FOCUS <br/>
       ON CREATING <span className="highlight1">EXCITING</span>, <span className="highlight2">ENGAGING</span>, <br/>
       AND <span className="highlight3">PERFORMATIVE</span> WEBSITES</h1>
+      <div className="initial-container">
+        <h1 className="initial">Eli B.</h1>
+      </div>
     </div>
   );
 };
