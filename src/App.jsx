67a83ea38b7592b574bc0d274c9293e3b264eb92
ebanchik <ebanchik@gmail.com>
2024-04-08 23:59:31 +0100

    projects page init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 // import { Banner } from './Banner'
 import { Banner } from './Banner'
+import { Projects } from '.Project'
 
 function App() {
   return (
   <div>
     <Banner />
+    <Projects />
   </div>
   )
 }
