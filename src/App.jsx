3029467391a0b5667347bc878f2fd233b9975e37
ebanchik <ebanchik@gmail.com>
2024-04-11 23:24:33 +0200

    add about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Banner } from './Banner'
 import { Projects } from './Projects'
+import { About } from './About'
 
 function App() {
   return (
   <div>
     <Banner />
     <Projects />
+    <About />
   </div>
   )
 }
