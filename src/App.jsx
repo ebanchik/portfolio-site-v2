4235bf6224936417d756ddbd33ec127ee308ed52
ebanchik <ebanchik@gmail.com>
2024-04-14 16:47:44 +0100

    snap-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { Banner } from './Banner'
 import { Projects } from './Projects'
 import { About } from './About'
 
+
 function App() {
   return (
-  <div>
-    <Banner />
-    <Projects />
-    <About />
-  </div>
-  )
+    <div className="snap-container">
+      <div className="snap-section"><Banner /></div>
+      <div className="snap-section"><Projects /></div>
+      <div className="snap-section"><About /></div>
+    </div>
+  );
 }
+
 
 export default App
